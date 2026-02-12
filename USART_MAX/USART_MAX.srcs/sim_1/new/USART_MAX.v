ebe3f092fe53629130d2588113361f1c6979f1e9
rzy4080-hash <rzy4080@icloud.com>
2026-02-12 11:47:01 +0800

    原始文件整理
    原始文件整理

---
+++ USART_MAX/USART_MAX.srcs/sim_1/new/USART_MAX.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2026/02/09 10:51:08
+// Design Name: 
+// Module Name: USART_MAX
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module USART_MAX(
+
+    );
+endmodule
